76da7681fdee6baa5aa5f500a69c9e0098fc345f
chethanbtn <31713087+Deviation-From-The-Norm@users.noreply.github.com>
2025-10-04 10:52:27 -0500

    added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigRed2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BigRed2.java
@@ -10,15 +10,16 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-@Autonomous(name="Red Big Leave ver 2", group="Robot")
+@Autonomous(name="Red Big Leave", group="Robot")
 public class BigRed2 extends AutoTemplate {
+    //This auto scores 3 points by leaving the launch line and getting ready to shoot
     @Override
     public void runOpMode() {
         setupAuto(); // Sets up things like the hardware map and motor modes (edit AutoTemplate when you change robot components)
 
-        driveStraight(DRIVE_SPEED, 33.0, 0.0);    // Drive Forward 36"
-        turnToHeading(TURN_SPEED, -155.0);        // Turn  CW to 90 Degrees
-        holdHeading(TURN_SPEED, -155.0, 1.0);   // Hold 90 Deg heading for a 1/2 second
+        driveStraight(DRIVE_SPEED, 33.0, 0.0);    // Drive Forward 33"
+        turnToHeading(TURN_SPEED, -155.0);        // Turn CW to -155 Degrees
+        holdHeading(TURN_SPEED, -155.0, 1.0);   // Hold -155 Deg heading for a 1 second
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
